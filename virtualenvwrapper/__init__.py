70a687f9966de0fb1b7b1a89f9113dbd264cdd9f
Doug Hellmann <doug.hellmann@gmail.com>
2009-05-30 15:58:52 -0400

    cannot run package from command line, so just warn on import

--- virtualenvwrapper/__init__.py
+++ virtualenvwrapper/__init__.py
@@ -1,11 +1,6 @@
 """virtualenvwrapper module
 """
 
-if __name__ == '__main__':
-    import os
-    import webbrowser
-    docs_root = os.path.join(os.path.dirname(__file__), 'docs', 'index.html')
-    webbrowser.open_new('file://' + docs_root)
-else:
-    import warnings
-    warnings.warn('Use virtualenvwrapper_bashrc to set up your shell environment.')
+import warnings
+docs_root = os.path.join(os.path.dirname(__file__), 'docs', 'index.html')
+warnings.warn('Use virtualenvwrapper_bashrc to set up your shell environment.  See %s for details.' % docs_root)
