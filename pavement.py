d07dcf8130f15d28a85968ec39257ed5b6ce4d2e
Doug Hellmann <doug.hellmann@gmail.com>
2009-08-13 08:31:43 -0400

    prepare release

--- pavement.py
+++ pavement.py
@@ -27,7 +27,7 @@ except:
 
 # What project are we building?
 PROJECT = 'virtualenvwrapper'
-VERSION = '1.19'
+VERSION = '1.20'
 os.environ['VERSION'] = VERSION
 
 # Read the long description to give to setup
