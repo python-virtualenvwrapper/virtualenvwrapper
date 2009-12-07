1fe262a22c257114771d638b45f0c56abb02d867
Doug Hellmann <doug.hellmann@gmail.com>
2009-12-07 08:58:26 -0500

    bump version

--- pavement.py
+++ pavement.py
@@ -27,7 +27,7 @@ except:
 
 # What project are we building?
 PROJECT = 'virtualenvwrapper'
-VERSION = '1.21'
+VERSION = '1.22'
 os.environ['VERSION'] = VERSION
 
 # Read the long description to give to setup
