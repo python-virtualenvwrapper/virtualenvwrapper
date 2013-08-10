fdfab8cbbd454419144d125371c6e3f67b49f4a7
Doug Hellmann <doug.hellmann@gmail.com>
2013-08-10 12:44:24 -0400

    consolidate 'ls' tests
    Change-Id: I11402fbb2672ae276652fdc415d5b8ee7cdafa84

--- tests/test_ls.sh
+++ tests/test_ls.sh
@@ -44,12 +44,34 @@ test_lssitepackages_add2virtualenv () {
     deactivate
 }
 
-test_no_workon_home () {
+test_lssitepackages_no_workon_home () {
     old_home="$WORKON_HOME"
     export WORKON_HOME="$WORKON_HOME/not_there"
+    rm -rf "$WORKON_HOME"
     lssitepackages >"$old_home/output" 2>&1
     output=$(cat "$old_home/output")
     assertTrue "Did not see expected message" "echo $output | grep 'does not exist'"
+    WORKON_HOME="$old_home"
+}
+
+test_lsvirtualenv_no_workon_home () {
+    old_home="$WORKON_HOME"
+    export WORKON_HOME="$WORKON_HOME/not_there"
+    rm -rf "$WORKON_HOME"
+    lsvirtualenv >"$old_home/output" 2>&1
+    output=$(cat "$old_home/output")
+    assertTrue "Did not see expected message" "echo $output | grep -q 'does not exist'"
+    WORKON_HOME="$old_home"
+}
+
+test_lsvirtualenv_space_in_workon_home () {
+    old_home="$WORKON_HOME"
+    export WORKON_HOME="$WORKON_HOME/with space"
+    mkdir "$WORKON_HOME"
+    (cd "$WORKON_HOME"; virtualenv testenv) 2>&1
+    lsvirtualenv -b >"$old_home/output"
+    output=$(cat "$old_home/output")
+    assertTrue "Did not see expected message in \"$output\"" "echo $output | grep -q 'testenv'"
     WORKON_HOME="$old_home"
 }
 
