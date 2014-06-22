f163dd3d82d72351b2da0497c38db7ec19042d4f
Doug Hellmann <doug@doughellmann.com>
2014-06-22 15:18:05 -0400

    Add tests for wipenv with editable packages
    Addresses bug #211
    
    Change-Id: I6051e53034f4251e4bdeaa36d7f1d3187c8b2501

--- tests/test_wipeenv.sh
+++ tests/test_wipeenv.sh
@@ -12,7 +12,7 @@ setUp () {
 
 tearDown() {
     if type deactivate >/dev/null 2>&1
-    then 
+    then
         deactivate
     fi
     rm -rf "$WORKON_HOME"
@@ -21,6 +21,38 @@ tearDown() {
 test_wipeenv () {
     mkvirtualenv "wipetest" >/dev/null 2>&1
     (cd tests/testpackage && python setup.py install) >/dev/null 2>&1
+    before="$(pip freeze)"
+    assertTrue "testpackage not installed" "pip freeze | grep testpackage"
+    wipeenv >/dev/null 2>&1
+    after="$(pip freeze)"
+    assertFalse "testpackage still installed" "pip freeze | grep testpackage"
+}
+
+test_wipeenv_pip_e () {
+    mkvirtualenv "wipetest" >/dev/null 2>&1
+    (cd tests/testpackage && pip install -e .) >/dev/null 2>&1
+    before="$(pip freeze)"
+    assertTrue "testpackage not installed" "pip freeze | grep testpackage"
+    wipeenv >/dev/null 2>&1
+    after="$(pip freeze)"
+    assertFalse "testpackage still installed" "pip freeze | grep testpackage"
+}
+
+# test_wipeenv_pip_e_url () {
+#     mkvirtualenv "wipetest" >/dev/null 2>&1
+#     (cd tests/testpackage && pip install -e 'git+https://github.com/kennethreitz/legit.git@3c4d3214811c7892edf903682fdbb44f4050b99a#egg=legit-origin')
+# # >/dev/null 2>&1
+#     before="$(pip freeze)"
+#     pip freeze
+#     assertTrue "legit not installed" "pip freeze | grep legit"
+#     wipeenv >/dev/null 2>&1
+#     after="$(pip freeze)"
+#     assertFalse "legit still installed" "pip freeze | grep legit"
+# }
+
+test_wipeenv_develop () {
+    mkvirtualenv "wipetest" >/dev/null 2>&1
+    (cd tests/testpackage && python setup.py develop) >/dev/null 2>&1
     before="$(pip freeze)"
     assertTrue "testpackage not installed" "pip freeze | grep testpackage"
     wipeenv >/dev/null 2>&1
