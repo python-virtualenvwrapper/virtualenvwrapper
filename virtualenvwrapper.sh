63596865a18093dff7707687a68491aeb5c5e8e0
Audrey Dutcher <audrey@rhelmot.io>
2018-11-03 22:00:39 -0700

    Make mkvirtualenv work with interpreters whose paths have spaces

--- virtualenvwrapper.sh
+++ virtualenvwrapper.sh
@@ -461,7 +461,7 @@ function mkvirtualenv {
         i=$(( $i + 1 ))
     done
 
-    if [ ! -z $interpreter ]
+    if [ ! -z "$interpreter" ]
     then
         out_args=( "--python=$interpreter" ${out_args[@]} )
     fi;
