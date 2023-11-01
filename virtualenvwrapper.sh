7f552fb0e2022c2936dcb91507f06ec295b8b221
bendikro <bro.devel@gmail.com>
2023-11-01 15:53:08 +0100

    Fix shell completion on FreeBSD
    sed on FreeBSD does not support '\s', and gives the error:
    "sed: 1: "/^\s*$/d": RE error: trailing backslash (\)"
    
    Fix by using POSIX bracket expression '[[:space:]]'

--- virtualenvwrapper.sh
+++ virtualenvwrapper.sh
@@ -586,7 +586,7 @@ function virtualenvwrapper_show_workon_options {
         | command \tr "\n" " " \
         | command \sed "s|/$VIRTUALENVWRAPPER_ENV_BIN_DIR/activate |/|g" \
         | command \tr "/" "\n" \
-        | command \sed "/^\s*$/d" \
+        | command \sed "/^[[:space:]]*$/d" \
         | (unset GREP_OPTIONS; command \grep -E -v '^\*$') 2>/dev/null
 }
 
